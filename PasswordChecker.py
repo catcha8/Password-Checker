88ae6369452be85756c3e06d89781c7d3a256ec3
Noémie <102274923+Name-shitty-github-profile@users.noreply.github.com>
2022-10-22 04:29:00 +0200

    Update PasswordChecker.py

--- PasswordChecker.py
+++ PasswordChecker.py
@@ -3,10 +3,8 @@ import hashlib
 from pystyle import *
 
 def request_api_data(query_char):
-    url = 'https://api.pwnedpasswords.com/range/' + query_char
-    res = requests.get(url)
-    if res.status_code != 200:
-        raise RuntimeError(f'Error fetching: {res.status_code}, check api and try again')
+    res = requests.get(f'https://api.pwnedpasswords.com/range/{query_char}')
+    if res.status_code != 200: raise RuntimeError(f'Error fetching: {res.status_code}, check api and try again')
     return res
 
 
@@ -21,13 +19,11 @@ def get_password_leaks_count(hashes, hash_to_check):
 
 def pwned_api_check(password):
     sha1password = hashlib.sha1(password.encode('utf-8')).hexdigest().upper()
-    first5_char, tail = sha1password[:5], sha1password[5:]
-    response = request_api_data(first5_char)
-    return get_password_leaks_count(response, tail)
+    response = request_api_data(sha1password[:5])
+    return get_password_leaks_count(response, sha1password[5:])
 
 
 def main(password):
-
     count = pwned_api_check(password)
     if count:
         Write.Print(f'{password} was found {count} times... You should probably change your password.', Colors.purple_to_red, interval=000.05)
@@ -36,12 +32,4 @@ def main(password):
     return 'All passwords checked!'
 
 
-def get_pass_list():
-        return main(Write.Input("Enter your password > ", Colors.red_to_green, interval=000.10))
-
-
-if __name__ == '__main__':
-    get_pass_list()
-
-
-input()
+main(Write.Input("Enter your password > ", Colors.red_to_green, interval=000.10))
